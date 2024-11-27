b4c6fd0c6a0f6a69cc4d4c648ecc796fed00ce2c
Sophia Regnault <90007008+EchoEcho33@users.noreply.github.com>
2024-11-27 15:41:13 -0500

    Modal Window

---
+++ Scripts/Modal.js
@@ -0,0 +1,7 @@
+var modal = document.getElementById("modalWindow");
+
+var openButton = document.getElementById("openButton");
+
+openButton.onclick = function() {
+    modal.style.display = "block";
+}
